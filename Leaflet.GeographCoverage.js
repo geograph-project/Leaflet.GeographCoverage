7ebbe5708dfc3cde60e802165f48770cafd80a03
barry <1413156+barryhunter@users.noreply.github.com>
2026-03-24 15:57:41 +0000

    New version that intergrates using GeographScout (optional)
    GeographScout implements a nice Hectad based cache, which avoids need to make requests for every map move.
    
    Ideally would move the centsquare version to use a similar tile-like cache too!

--- Leaflet.GeographCoverage.js
+++ Leaflet.GeographCoverage.js
@@ -40,7 +40,8 @@ L.GeographCoverage = L.FeatureGroup.extend({
 		squareScale: 'square',
 		query: '',
 		user_id: null,
-		opacity: 1
+        opacity: 1,
+        scoutLayer: null //can link in the scout layer (even if not on the map!)
 	},
 
 	initialize: function (options) {
@@ -52,69 +53,128 @@ L.GeographCoverage = L.FeatureGroup.extend({
 	},
 
 	//stolen from https://github.com/MatthewBarker/leaflet-wikipedia/blob/master/source/leaflet-wikipedia.js
-        /**
-            Store a reference to the map and call the requestData() method.
-            @private
-        */
-        onAdd: function (map) {
-            map.on('moveend', this.requestData, this);
+    /**
+        Store a reference to the map and call the requestData() method.
+        @private
+    */
+    onAdd: function (map) {
+        map.on('moveend', this.requestData, this);
 
-	    //we dont can't add circles to markerPane, as the not really clickable markers, in normal markerPane, may overlap real clickable markers!
+        //we dont can't add circles to markerPane, as the not really clickable markers, in normal markerPane, may overlap real clickable markers!
 
-            var pane = map.getPane('coveragePane') || map.createPane('coveragePane');
-	    pane.style.zIndex = 300; //above tilePane, but below shadowPane
-	    pane.style.pointerEvents = 'none';  //see https://leafletjs.com/examples/map-panes/
-  	    L.DomUtil.setOpacity(pane, this.options.opacity);
+        var pane = map.getPane('coveragePane') || map.createPane('coveragePane');
+        pane.style.zIndex = 300; //above tilePane, but below shadowPane
+        pane.style.pointerEvents = 'none';  //see https://leafletjs.com/examples/map-panes/
+        L.DomUtil.setOpacity(pane, this.options.opacity);
 
-            this._map = map;
-            this.requestData();
-        },
-        /**
-            Remove the 'moveend' event listener and clear all the markers.
-            @private
-        */
-        onRemove: function (map) {
-            map.off('moveend', this.requestData, this);
+        this._map = map;
+        this.requestData();
+
+		if (this.options.scoutLayer) {
+		    this.options.scoutLayer.on('dataupdated', this.requestData, this);
+		}
+
+    },
+
+    /**
+        Remove the 'moveend' event listener and clear all the markers.
+        @private
+    */
+    onRemove: function (map) {
+        map.off('moveend', this.requestData, this);
+        this.clearLayers();
+        this._circles = [];
+        this._labels = [];
+	    this.outputStatus('');
+
+		if (this.options.scoutLayer) {
+		    this.options.scoutLayer.off('dataupdated', this.requestData, this);
+		}
+    },
+
+    /**
+     Redraws the coverage, call this if you change the query, user_id, or even squareScale in options
+     @public
+    */
+    Reset: function () {
+        this.clearLayers();
+        this._circles = [];
+        this._labels = [];
+        this.requestData();
+        return this;
+    },
+
+    setOpacity: function (opacity) {
+        this.options.opacity = opacity;
+        var pane = map.getPane('coveragePane') || map.createPane('coveragePane');
+        L.DomUtil.setOpacity(pane, this.options.opacity);
+        return this;
+    },
+
+    /**
+        Send a query request for JSONP data.
+        @private
+    */
+    requestData: function () {
+        var zoom = this._map.getZoom(),
+        origin = this._map.getCenter(),
+	    bounds = map.getBounds(),
+	    self = this;
+
+        // 1. Basic Zoom Guard
+        if (zoom < this.options.minZoom || zoom > this.options.maxZoom) {
             this.clearLayers();
             this._circles = [];
             this._labels = [];
-	    this.outputStatus('');
-        },
+            this.outputStatus('');
+            return;
+        }
 
-        /**
-            Redraws the coverage, call this if you change the query, user_id, or even squareScale in options
-            @public
-        */
-	Reset: function () {
-                this.clearLayers();
-            	this._circles = [];
-	        this._labels = [];
-		this.requestData();
-		return this;
-	},
+        // 3. Determine Mode (Centisquare still uses legacy API)
+        var isHectadMode = !(this.options.squareScale === 'centisquare' || zoom >= 15);
 
-	setOpacity: function (opacity) {
-	        this.options.opacity = opacity;
-                var pane = map.getPane('coveragePane') || map.createPane('coveragePane');
-		L.DomUtil.setOpacity(pane, this.options.opacity);
-        	return this;
-	},
+        if (isHectadMode && this.options.scoutLayer) {
+            // If the Scout layer isn't currently added to the map, it won't be
+            // listening to 'moveend'. We trigger its logic manually.
+            if (!this._map.hasLayer(this.options.scoutLayer)) {
+                this.options.scoutLayer._onMapMove(null, this._map);
+            }
 
-        /**
-            Send a query request for JSONP data.
-            @private
-        */
-        requestData: function () {
-            var zoom = this._map.getZoom(),
-            origin = this._map.getCenter(),
-	    bounds = map.getBounds(),
-	    self = this;
-	    var data = {
-		olbounds: bounds.toBBoxString(),
-		q: this.options.query,
-		user_id: this.options.user_id
+            // Filter the shared cache for squares within the current map bounds
+            const cachedSquares = Object.values(this.options.scoutLayer._localSquareCache).filter(sq => {
+                // We use a quick bounds check or rely on the Hectad ID
+                // Here we check if the square's specific cached entry is within view
+                // Note: Scout's _localSquareCache uses Grid Refs as keys (e.g., 'TQ3039')
+                // We can approximate visibility or just render all cached items
+                // that Scout has loaded for the current view.
+                if (!sq.lat)
+      	        this._getCenterFromGR(sq); //stores it directly
+                return bounds.contains([sq.lat, sq.lng]);
+            });
+
+            var resolution = 1000;
+            var height = map.distance(bounds.getSouthWest(),bounds.getNorthWest());
+            this._extra_class = ((height/resolution) < 8)?'-large':'';
+
+            if (cachedSquares.length > 0) {
+                this.parseData({ markers: cachedSquares });
+                return;
+            }
+
+            // If cache is empty but Scout is loading, parseData will be called
+            // eventually when Scout's promises resolve and this function re-triggers.
+            this.outputStatus("Syncing with Scout cache...");
+            return;
+        }
+
+        // --- FALLBACK: Legacy JSONP for Centisquares or when Scout is unavailable ---
+        var data = {
+	    	olbounds: bounds.toBBoxString(),
+		    q: this.options.query || '',
+    		user_id: (this.options.user_id > 0)?this.options.user_id:'' //specifically send null as empty string
 	    };
 
+        var resolution;
 		if (this.options.squareScale == 'centisquare' || zoom >= 15) {
 			var url = "https://www.geograph.org.uk/stuff/squares-centi.json.php";
 
@@ -130,63 +190,97 @@ L.GeographCoverage = L.FeatureGroup.extend({
 				if (vgr && vgr.length >0) myriads.push(vgr);
 				data.myriads = myriads.join(',');
 			}
-			var resolution = 100;
+            resolution = 100;
 		} else {
 			var url = "https://www.geograph.org.uk/stuff/squares.json.php";
 			myriads = new Array();
-			var resolution = 1000;
+            resolution = 1000;
 		}
 
-		var height = map.distance(bounds.getSouthWest(),bounds.getNorthWest());
-		this._extra_class = ((height/resolution) < 8)?'-large':'';
+        var height = map.distance(bounds.getSouthWest(),bounds.getNorthWest());
+        this._extra_class = ((height/resolution) < 8)?'-large':'';
 
-            if (zoom >= this.options.minZoom && zoom <= this.options.maxZoom) {
-		var ajaxRequest = (window.reqwest)?reqwest:$.ajax;
-                ajaxRequest({
-                    url: url+"?callback=?",
+
+        if (zoom >= this.options.minZoom && zoom <= this.options.maxZoom) {
+		    if (window.fetch) {
+			    var params = new URLSearchParams(data);
+                var self = this;
+
+                fetch(url+'?'+params.toString())
+                    .then(response => {
+                        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+                        return response.json(); // This returns a promise containing the JSON
+                    })
+                    .then(jsonResponse => {
+                        // This is your 'success' callback
+                        self.parseData(jsonResponse);
+                    })
+                    .catch(err => {
+                        // This is your 'error' callback
+                        console.error("GeographCoverage fetch failed:", err);
+                        self.outputStatus("Error loading data.");
+                    });
+
+	    	} else {
+    			var ajaxRequest = (window.reqwest)?reqwest:$.ajax;
+    	        ajaxRequest({
+            	    url: url,
                     data: data,
-		    type: 'jsonp',
-                    success: function (response) { self.parseData(response); }
+			        type: 'json',
+            	    success: function (response) { self.parseData(response); }
                 });
-            } else {
-                this.clearLayers();
+	        }
+        } else {
+            this.clearLayers();
             this._circles = [];
             this._labels = [];
-  	    this.outputStatus('');
-            }
-	},
+            this.outputStatus('');
+        }
+ 	},
 
-        getMyriadLetter: function(latLng) {
+    getMyriadLetter: function(latLng) {
 		if (!window.GT_WGS84) //only works if GeoTools2 is loaded!
 			return false;
-                var gridref = null;
-                var wgs84=new GT_WGS84();
-                wgs84.setDegrees(latLng.lat,latLng.lng);
+        var gridref = null;
+        var wgs84=new GT_WGS84();
+        wgs84.setDegrees(latLng.lat,latLng.lng);
 
-                if (wgs84.isIreland2())
-                       grid=wgs84.getIrish(true);
-                else
-                       grid=wgs84.getOSGB();
+        if (wgs84.isIreland2())
+            grid=wgs84.getIrish(true);
+        else
+            grid=wgs84.getOSGB();
 
-                if (grid && grid.status && grid.status == 'OK') {
-                        var gridref = grid.getGridRef(1);
-                        var bits = gridref.split(/ /);
-                        return  bits[0];
-                } else
-                        return false;
-        },
+        if (grid && grid.status && grid.status == 'OK') {
+            var gridref = grid.getGridRef(1);
+            var bits = gridref.split(/ /);
+            return  bits[0];
+        } else
+            return false;
+    },
 
-	outputStatus: function (text) {
-		document.getElementById('message').innerHTML = text;
-		return this;
-	},
+    _getCenterFromGR: function(sq) {
+        // Quick helper to approximate lat/lng from GR if the Scout cache
+        // didn't include explicit coordinates (the Hectad API often doesn't)
+        let grid = (sq.gr.length === 6) ? new GT_OSGB() : new GT_Irish();
+        grid.parseGridRef(sq.gr);
+        // Add 500m to get to the center of the 1km square
+        grid.setGridCoordinates(grid.eastings + 500, grid.northings + 500);
+        let conv = grid.getWGS84(true);
+        sq.lat = conv.latitude, sq.lng = conv.longitude;
+    },
 
-	/**
-            Parse the response data and call the addMarker() method for each result.
-            @param {Object} response - JSON data
-            @private
-        */
-        parseData: function (data) {
+    outputStatus: function (text) {
+        let ele = document.getElementById('message');
+   		if (ele) ele.innerHTML = text;
+        return this;
+    },
+
+    /**
+        Parse the response data and call the addMarker() method for each result.
+        @param {Object} response - JSON data
+        @private
+    */
+    parseData: function (data) {
 
 		if (data.error && data.error.length > 0) {
 			this.outputStatus(data.error);
@@ -196,75 +290,72 @@ L.GeographCoverage = L.FeatureGroup.extend({
 		}
 
 		//flag all current markers as old
-			for (i in this._labels) 
-				if (this._labels[i] != null) {
-					this._labels[i].old = true;
-				}
+		for (i in this._labels)
+			if (this._labels[i] != null) {
+				this._labels[i].old = true;
+			}
 
-		        var loaded = 0;
+	        var loaded = 0;
 
-		this.outputStatus("Adding "+(data.markers.length)+" Squares...");
+        this.outputStatus("Adding "+(data.markers.length)+" Squares...");
 
-			for (var i = 0; i < data.markers.length; i++) {
-				id = data.markers[i].gr+this._extra_class;
-				if (this._labels[id] && this._labels[id] != null) {
-			            this._labels[id].old = false;
-                                } else {
-  					var labelPos = [parseFloat(data.markers[i].lat),parseFloat(data.markers[i].lng)];
+		for (var i = 0; i < data.markers.length; i++) {
+			id = data.markers[i].gr+this._extra_class;
+			if (this._labels[id] && this._labels[id] != null) {
+		        this._labels[id].old = false;
+            } else {
+				var labelPos = [parseFloat(data.markers[i].lat),parseFloat(data.markers[i].lng)];
 
-					//this._circles[id] = L.circleMarker(labelPos, {radius: 10});
+				//this._circles[id] = L.circleMarker(labelPos, {radius: 10});
 
-					//if ((data.markers[i].c > 999 && zoom == 13) || (data.markers[i].c > 99 && zoom == 15))
-					//	data.markers[i].c = '<div style="transform:rotate(-35deg)">'+data.markers[i].c.toString()+'</div>';
-					if (data.markers[i].c > 999)
-						data.markers[i].c = '<div style="font-size:0.7em">'+data.markers[i].c.toString()+'</div>';
-					else if (data.markers[i].c > 99)
-						data.markers[i].c = '<div style="font-size:0.85em">'+data.markers[i].c.toString()+'</div>';
+                //if ((data.markers[i].c > 999 && zoom == 13) || (data.markers[i].c > 99 && zoom == 15))
+                //      data.markers[i].c = '<div style="transform:rotate(-35deg)">'+data.markers[i].c.toString()+'</div>';
+                if (data.markers[i].c > 999)
+                    data.markers[i].c = '<div style="font-size:0.7em">'+data.markers[i].c.toString()+'</div>';
+                else if (data.markers[i].c > 99)
+                    data.markers[i].c = '<div style="font-size:0.85em">'+data.markers[i].c.toString()+'</div>';
 
-					if (data.markers[i].g != undefined && data.markers[i].g==0)
-						data.markers[i].c = '<div style="color:gray">'+data.markers[i].c.toString()+'</div>';
+                if (data.markers[i].g != undefined && data.markers[i].g==0)
+                    data.markers[i].c = '<div style="color:gray">'+data.markers[i].c.toString()+'</div>';
 
-					this._labels[id] = L.marker(labelPos, {
-			                        icon: L.divIcon({ 
-							iconSize: [40,40],
-							html: data.markers[i].c.toString(),
-							className: (data.markers[i].r?'coverage-marker-normal':'coverage-marker-highlight')+this._extra_class
-						}),
-						pane: 'coveragePane',
-			                        title: data.markers[i].gr
-			                });
+				this._labels[id] = L.marker(labelPos, {
+		            icon: L.divIcon({
+						iconSize: [40,40],
+						html: data.markers[i].c.toString(),
+                        className: (data.markers[i].r?'coverage-marker-normal':'coverage-marker-highlight')+this._extra_class
+					}),
+                    pane: 'coveragePane',
+		            title: data.markers[i].gr
+		        });
 
-					//this.addLayer(this._circles[id]);
-					this.addLayer(this._labels[id]);
-			        }
-		                loaded=loaded+1;
-		        }
-
+				//this.addLayer(this._circles[id]);
+				this.addLayer(this._labels[id]);
+		    }
+	        loaded=loaded+1;
+	    }
 
 		this.outputStatus("Removing Old Squares...");
-			for (i in this._labels) 
-				if (this._labels[i] != null) 
-					if (this._labels[i].old == true) {
-						//this.removeLayer(this._circles[i]);
-						this.removeLayer(this._labels[i]);
-						this._labels[i] = null;		
-						this._circles[i] = null;		
-					}
-
-			
-			
-			if (data.count && data.count.length > 0) {
-				if (data.markers.length == data.count) {
-					this.outputStatus("Finished, showing "+data.markers.length+" squares.");
-				} else {
-					this.outputStatus("Finished, showing "+data.markers.length+" of "+data.count+" squares.");
+		for (i in this._labels)
+			if (this._labels[i] != null)
+				if (this._labels[i].old == true) {
+					//this.removeLayer(this._circles[i]);
+					this.removeLayer(this._labels[i]);
+					this._labels[i] = null;
+					this._circles[i] = null;
 				}
-			} else {
-				this.outputStatus("Finished, showing "+data.markers.length+" squares.");
-			}
-			running = false;
 
-        }
+		if (data.count && data.count.length > 0) {
+			if (data.markers.length == data.count) {
+				this.outputStatus("Finished, showing "+data.markers.length+" squares.");
+			} else {
+				this.outputStatus("Finished, showing "+data.markers.length+" of "+data.count+" squares.");
+			}
+		} else {
+			this.outputStatus("Finished, showing "+data.markers.length+" squares.");
+		}
+		running = false;
+
+    }
 
 });
 
